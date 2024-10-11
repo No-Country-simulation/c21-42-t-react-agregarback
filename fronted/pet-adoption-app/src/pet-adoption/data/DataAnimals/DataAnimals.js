c765a47b73927786ef8e9f8ec25f848b302ee301
Ronald19990527 <ronaldhernandezdp@unimagdalena.edu.co>
2024-10-11 12:16:15 -0500

    adding CardAnimal, ContainerCardsAnimals and DataAnimals

---
+++ fronted/pet-adoption-app/src/pet-adoption/data/DataAnimals/DataAnimals.js
@@ -0,0 +1,117 @@
+export const DataAnimals = {
+    dogs: [
+        {
+            name: "Mico",
+            image: "https://static.fundacion-affinity.org/cdn/farfuture/PVbbIC-0M9y4fPbbCsdvAD8bcjjtbFc0NSP3lRwlWcE/mtime:1643275542/sites/default/files/los-10-sonidos-principales-del-perro.jpg",
+            gender: "Masculino",
+            age: 10,
+            size: "Mediano"
+        },
+        {
+            name: "Chispa",
+            image: "https://s1.elespanol.com/2021/11/10/actualidad/626198188_214456909_1024x576.jpg",
+            gender: "Masculino",
+            age: 13,
+            size: "Mediano"
+        },
+        {
+            name: "Rayo",
+            image: "https://as01.epimg.net/diarioas/imagenes/2022/05/29/actualidad/1653826510_995351_1653826595_noticia_normal_recorte1.jpg",
+            gender: "Masculino",
+            age: 15,
+            size: "Mediano"
+        },
+        {
+            name: "Chiqui",
+            image: "https://media.traveler.es/photos/613760adcb06ad0f20e11980/master/w_1600%2Cc_limit/202931.jpg",
+            gender: "Masculino",
+            age: 11,
+            size: "Grande"
+        },
+        {
+            name: "Plutón",
+            image: "https://estaticos-cdn.prensaiberica.es/clip/823f515c-8143-4044-8f13-85ea1ef58f3a_16-9-discover-aspect-ratio_default_0.jpg",
+            gender: "Masculino",
+            age: 13,
+            size: "Grande"
+        },
+        {
+            name: "Chico",
+            image: "https://www.elindependiente.com/wp-content/uploads/2024/04/perro-inteligente.jpg",
+            gender: "Masculino",
+            age: 16,
+            size: "Grande"
+        },
+        {
+            name: "Luna",
+            image: "https://media.es.wired.com/photos/65845b5ea4076464da362974/16:9/w_2560%2Cc_limit/Science-Life-Extension-Drug-for-Big-Dogs-Is-Getting-Closer-1330545769.jpg",
+            gender: "Femenino",
+            age: 4,
+            size: "Grande"
+        },
+        {
+            name: "Lola",
+            image: "https://content.nationalgeographic.com.es/medio/2023/12/29/ojos-perros-3_433a79bd_231229224215_800x800.jpg",
+            gender: "Femenino",
+            age: 7,
+            size: "Grande"
+        },
+        {
+            name: "Coco",
+            image: "https://www.elcomercio.com/wp-content/uploads/2023/05/perro--700x392.jpg",
+            gender: "Masculino",
+            age: 11,
+            size: "Grande"
+        },
+        {
+            name: "Linda",
+            image: "https://hips.hearstapps.com/hmg-prod/images/gettyimages-992299558-65fd23c618187.jpg?crop=1xw:0.8491394606103619xh;center,top&resize=1200:*",
+            gender: "Femenino",
+            age: 4,
+            size: "Grande"
+        },
+        {
+            name: "Noa",
+            image: "https://baluka.es/cdn/shop/articles/razas-de-perros-mas-comunes.jpg?v=1695689090",
+            gender: "Femenino",
+            age: 9,
+            size: "Pequeña"
+        },
+        {
+            name: "Nina",
+            image: "https://www.ngenespanol.com/wp-content/uploads/2022/08/estudio-ayuda-a-conocer-origen-de-los-perros.jpg",
+            gender: "Femenino",
+            age: 8,
+            size: "Pequeña"
+        },
+        {
+            name: "Toby",
+            image: "https://e00-expansion.uecdn.es/assets/multimedia/imagenes/2022/05/03/16515945858800.jpg",
+            gender: "Masculino",
+            age: 13,
+            size: "Pequeño"
+        },
+        {
+            name: "Rocky",
+            image: "https://static.laverdad.es/www/multimedia/202110/14/media/perrete.jpg",
+            gender: "Masculino",
+            age: 6,
+            size: "Pequeño"
+        },
+        {
+            name: "Thor",
+            image: "https://caracol.com.co/resizer/v2/GAULRJST6ZD25KV3BT66YOJ6PY.jpg?auth=f7cbfa1b66f8a2f2b61665732d48ee0c062809221ad9ef0b4984d43da14d5eb3&width=650&height=488&quality=70&smart=true",
+            gender: "Masculino",
+            age: 5,
+            size: "Pequeño"
+        },
+        {
+            name: "Simba",
+            image: "https://www.publico.es/ahorro-consumo-responsable/wp-content/uploads/2022/03/westhighland-1024x682.jpg",
+            gender: "Masculino",
+            age: 4,
+            size: "Pequeño"
+        }
+    ],
+    cats: []
+};
--- fronted/pet-adoption-app/src/pet-adoption/pet-adoption-pages/HomeAdoptPage/HomeAdoptPage.jsx
+++ fronted/pet-adoption-app/src/pet-adoption/pet-adoption-pages/HomeAdoptPage/HomeAdoptPage.jsx
@@ -1,12 +1,13 @@
-import Navbar from "../Navbar/Navbar";
+import ContainerCardsAnimals from "../../components/ContainerCardsAnimals/ContainerCardsAnimals";
+import Navbar from "../../components/Navbar/Navbar";
+import './HomeAdoptPage.css';
 
 const HomeAdoptPage = () => {
   return (
-    <div>
+    <div className="bg-[#F4F0EF]">
       <Navbar />
-<h1>Home Adopt Page</h1>
+      <ContainerCardsAnimals />
     </div>
-    
   );
 };
 
